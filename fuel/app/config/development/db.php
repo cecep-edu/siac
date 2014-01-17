dca307f48c18f51dced3abe1f826eab432c34744
Antonio Pardo <apardo@alabs.org>
2014-01-17 10:29:24 -0500

    acceso a db y permisos

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=siac_deveĺ',
 			'username'   => 'root',
 			'password'   => 'root',
 		),
